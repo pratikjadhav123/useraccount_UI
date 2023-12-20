891475f30ff4444d97232bb623930e4dc2e2a17d
pratikjadhav123 <118898674+pratikjadhav123@users.noreply.github.com>
2023-12-20 16:42:40 +0530

    Contact page is working and mail is being sent

--- src/Componants/Form/Form.jsx
+++ src/Componants/Form/Form.jsx
@@ -1,87 +1,37 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./Form.css"
 
 const Form = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
+  const form = useRef();
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    try {
-      // Replace 'YOUR_API_ENDPOINT' with your backend API endpoint
-      await axios.post("YOUR_API_ENDPOINT", formData);
-      console.log("Form submitted successfully!");
-      // Handle success or any necessary actions after successful form submission
-    } catch (error) {
-      console.error("Error submitting form:", error);
-      // Handle error, display error message, etc.
-    }
+
+    emailjs.sendForm('service_ow9b818', 'template_qjzk89v', form.current, 'DaOUlfQK-sYmU-bdN')
+      .then((result) => {
+          console.log(result.text);
+          alert("message sent");
+
+      }, (error) => {
+          console.log(error.text);
+      });
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <p className="h5 text-center mb-4">Write to us</p>
-        <div className="grey-text">
-          <label>
-            Your name:
-            <input
-              type="text"
-              name="name"
-              value={formData.name}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Your email:
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Subject:
-            <input
-              type="text"
-              name="subject"
-              value={formData.subject}
-              onChange={handleChange}
-            />
-          </label>
-          <label>
-            Your message:
-            <textarea
-              rows="2"
-              name="message"
-              value={formData.message}
-              onChange={handleChange}
-            ></textarea>
-          </label>
-        </div>
-        <div className="text-center">
-          <button type="submit">
-            Send
-            <span role="img" aria-label="paper-plane" className="ml-1">
-              ✈️
-            </span>
-          </button>
-        </div>
-      </form>
-    </div>
+    < >
+      <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Subject</label>
+      <input type="subject" name="user_subject" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+    </>
   );
 };
 
